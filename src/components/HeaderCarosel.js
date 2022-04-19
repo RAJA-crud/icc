43082fc86be1ad828cc2d1ae304142152ddd43f2
gangasankar <84890978+gangasankar@users.noreply.github.com>
2022-04-19 13:07:42 +0530

    header ui modify

--- src/components/HeaderCarosel.js
+++ src/components/HeaderCarosel.js
@@ -4,10 +4,11 @@ const HeaderCarosel = () => {
     return (
         <div className="header-carosel">
             <div className="header-text">
-                <span>Catogery Title | May22,2022</span>
-                <h1 >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </h1>
-                <div>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                <button style={{marginTop:"10px"}}>All Challenges</button>
+                {/* <span>Catogery Title | May22,2022</span> */}
+                <h1 style={{padding:"10px 0"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </h1>
+                <div style={{padding:"10px 0"}}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                <button  className="headerButton" style={{padding:"10px 0"}}>Read more</button>
+                {/* <div className="headerButton">All Changes</div> */}
             </div>
         </div>
     )
