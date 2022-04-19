0b49b55a4030115ba6981ab63d74426ec211ea00
Aswin-subramanian <aswin@crud-operations.com>
2022-04-19 15:13:16 +0530

    chessBanner section included

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Article from "./components/Article";
 import CallToAction from "./components/CallToAction";
 import Catogery from "./components/Catogery";
+import ChessBanner from "./components/ChessBanner";
 import HeaderCarosel from "./components/HeaderCarosel";
 import Panel_8 from "./components/Panel_8";
 import Panel_9 from "./components/Panel_9";
@@ -15,6 +16,7 @@ function App() {
       <CallToAction />
       <Article />
       {/* <Catogery /> */}
+      <ChessBanner />
       <Panel_8 />
       <Panel_9 />
     </div>
